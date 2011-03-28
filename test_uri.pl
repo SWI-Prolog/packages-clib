SWI-Prolog/packages-clib

commit a46f7a2d792957d12228ce36414cb2b89ef72be1

MODIFIED: When creating a normalized uri, use ESC_QVALUE to escape the query and fragment. This ensured escaping + as %2B, etc. I'm not reallt sure about these things.  Seems the normalization already does too much rewriting.  At least this avoids confusion around +.

uri.c
@@ -1151,11 +1151,11 @@ normalize_in_charbuf(charbuf *cb, uri_component_ranges *ranges, int iri)
   }
   if ( ranges->query.start )
   { add_charbuf(cb, '?');
-    add_range_charbuf(cb, &ranges->query, iri, ESC_QUERY);
+    add_range_charbuf(cb, &ranges->query, iri, ESC_QVALUE);
   }
   if ( ranges->fragment.start )
   { add_charbuf(cb, '#');
-    add_range_charbuf(cb, &ranges->fragment, iri, ESC_FRAGMENT);
+    add_range_charbuf(cb, &ranges->fragment, iri, ESC_QVALUE);
   }
 
   return TRUE;

commit cda2860bcff745eb09830ef7459f228a3d4014d8

FIXED: normalise_iri/2: escaped & and =.  Bug introduced in commit a46f7a2d792957d12228ce36414cb2b89ef72be1

uri.c
@@ -121,7 +121,7 @@ domain_error(const char *expected, term_t found)
 		 *******************************/
 
 #define	ESC_PATH       (CH_PCHAR|CH_EX_PATH)
-#define	ESC_QUERY      (CH_PCHAR|CH_EX_QF)
+#define	ESC_QUERY      (CH_UNRESERVED|CH_PSUBDELIM|CH_EX_PCHAR|CH_EX_QF)
 #define	ESC_QVALUE     (CH_UNRESERVED|CH_QSUBDELIM|CH_EX_PCHAR|CH_EX_QF)
 #define	ESC_QNAME      (CH_PCHAR)
 #define	ESC_FRAGMENT   (CH_PCHAR|CH_EX_QF)
@@ -142,7 +142,8 @@ domain_error(const char *expected, term_t found)
 #define CH_EX_QF      0x0080		/* Extra query and fragment chars */
 #define CH_EX_SCHEME  0x0100
 #define CH_QSUBDELIM  0x0200
-#define CH_EX_PATH    0x0400
+#define CH_PSUBDELIM  0x0400
+#define CH_EX_PATH    0x0800
 
 #define CH_SCHEME	(CH_ALPHA|CH_DIGIT|CH_EX_SCHEME)
 #define CH_UNRESERVED	(CH_ALPHA|CH_DIGIT|CH_EX_UNRES)
@@ -172,6 +173,7 @@ fill_flags()
     set_flags("-._~",        CH_EX_UNRES);
     set_flags(":/?#[]@",     CH_GENDELIM);
     set_flags("!$&'()+*,;=", CH_SUBDELIM);
+    set_flags("!$&'()*,;=",  CH_PSUBDELIM); /* = CH_SUBDELIM - "+"  */
     set_flags("!$'()*,",     CH_QSUBDELIM); /* = CH_SUBDELIM - "&=+" */
     set_flags(":@",          CH_EX_PCHAR);
     set_flags("/",           CH_EX_PATH);
@@ -1151,7 +1153,7 @@ normalize_in_charbuf(charbuf *cb, uri_component_ranges *ranges, int iri)
   }
   if ( ranges->query.start )
   { add_charbuf(cb, '?');
-    add_range_charbuf(cb, &ranges->query, iri, ESC_QVALUE);
+    add_range_charbuf(cb, &ranges->query, iri, ESC_QUERY);
   }
   if ( ranges->fragment.start )
   { add_charbuf(cb, '#');

test_uri.pl
@@ -71,6 +71,12 @@ test(normalise_iri, NormalIRI == 'example://a/b/c/%7Bfoo%7D') :-
 	uri_normalized_iri('eXAMPLE://a/./b/../b/%63/%7bfoo%7d', NormalIRI).
 test(normalise_iri, NormalIRI == 'http://a.b/a%3F?x') :-	% 3F = '?'
 	uri_normalized_iri('http://a.b/a%3f?x', NormalIRI).
+test(normalise_iri, NormalIRI == 'http://a.b/a?x=1&y=2') :-
+	uri_normalized_iri('http://a.b/a?x=1&y=2', NormalIRI).
+test(normalise_iri, NormalIRI == 'http://a.b/a?x=1&y=2#aap') :-
+	uri_normalized_iri('http://a.b/a?x=1&y=2#aap', NormalIRI).
+test(normalise_iri, NormalIRI == 'http://a.b/a?x=1&y=2#aap%20noot') :-
+	uri_normalized_iri('http://a.b/a?x=1&y=2#aap+noot', NormalIRI).
 
 :- end_tests(iri).
 