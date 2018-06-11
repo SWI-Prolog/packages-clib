4c22509dd42fb8d28aaa67973a5b69b31841f3ab
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-06-11 10:52:42 +0200

    Deleted dead code

--- udp_broadcast.pl
+++ udp_broadcast.pl
@@ -263,31 +263,6 @@ and subtle differences that must be taken into consideration:
     udp_unicast_join_hook/3,                    % +Scope, +From, +Data
     black_list/1.                               % +Term
 
-%!   ~>(:P, :Q) is nondet.
-%
-%    asserts temporal Liveness (something good  happens, eventually) and
-%    Safety (nothing bad ever  happens)   properties.  Analogous  to the
-%    "leads-to" operator of Owicki and Lamport, 1982. Provides a sort of
-%    lazy implication described informally as:
-%
-%      - Liveness: For all possible outcomes, P -> Q, eventually.
-%      - Safety: For all possible outcomes, (\+P ; Q), is invariant.
-%
-%    Described practically:
-%
-%    P ~> Q, declares that if P is true, then Q must be true, now or at
-%    some point in the future.
-
-:- meta_predicate ~>(0,0).
-:- op(950, xfy, ~>).
-
-~>(P, Q) :-
-    setup_call_cleanup(P,
-                       (true; fail),
-                       (   Q -> true
-                       ;   throw(error(goal_failed(Q), context(~>, _))))
-                       ).
-
 :- meta_predicate safely(0).
 
 safely(Predicate) :-
