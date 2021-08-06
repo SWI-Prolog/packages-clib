731afc4c590ddc73d625e48c4d6d992fa7d1319a
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-08-06 11:23:18 +0200

    ADDED: uuid_property/2 to get the version and (for 1 and 2) time stamp of the UUID.

--- uuid.pl
+++ uuid.pl
@@ -35,7 +35,8 @@
 
 :- module(uuid,
           [ uuid/1,                     % -UUID
-            uuid/2                      % -UUID, +Options
+            uuid/2,                     % -UUID, +Options
+            uuid_property/2             % +UUID, ?Property
           ]).
 
 /** <module> Universally Unique Identifier (UUID) Library
@@ -54,6 +55,7 @@ versions.  Some typical calls are given below:
 
 @tbd Compare UUIDs, extract time and version from UUIDs
 @see http://www.ossp.org/pkg/lib/uuid/
+@see https://en.wikipedia.org/wiki/Universally_unique_identifier
 */
 
 link_uuid :-
@@ -128,3 +130,37 @@ random_uuid(UUID) :-
             ~`0t~16r~4+-~|\c
             ~`0t~16r~12+', [A,B,C,D,E]).
 :- endif.
+
+%!  uuid_property(+UUID, ?Property)
+%
+%   True when UUID is a property of the given UUID. Supported properties
+%   are:
+%
+%     - version(V)
+%       Return the version of the UUID (1..5)
+%     - time(-Stamp)
+%       Time using SWI-Prolog's time stamp (float with seconds
+%       since January 1, 1970, UTC).  Only for version 1 and 2
+%       UUIDs
+%
+%   @tbd Implement more properties.
+
+uuid_property(UUID, P) :-
+    property_uuid(P, UUID).
+
+property_uuid(version(V), UUID) :-
+    split_string(UUID, "-", "", [_A,_B,C,_D,_E]),
+    sub_string(C, 0, 1, _, F),
+    char_type(F, xdigit(V)).
+property_uuid(time(Time), UUID) :-
+    split_string(UUID, "-", "", [A,B,C,_D,_E]),
+    sub_atom(C, 0, 1, _, F),
+    has_time(F),
+    sub_string(C, 1, _, 0, T),
+    atomics_to_string(["0x",T,B,A], Hex),
+    number_string(Nanos, Hex),
+    Offset is 24*60*60*141427*10 000 000,
+    Time is (Nanos-Offset)/10000000.0.
+
+has_time('1').
+has_time('2').
