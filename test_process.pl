0dd87615e792b8cc7eddc33bf9d1d4813000da7a
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2022-03-29 17:30:07 +0200

    TEST: Cross-platform null in stream_input

--- test_process.pl
+++ test_process.pl
@@ -80,11 +80,9 @@ test(null_error, [condition(has_exe(sh))]) :-
                    ['-c', 'echo "THIS IS AN ERROR" 1>&2'],
                    [stderr(null)]).
 test(stream_input, [condition(has_exe(wc)), X == "0"]) :-
-    open('/dev/null', read, In, [type(binary)]),
     process_create(path(wc),
                    ['-c'],
-                   [stdin(stream(In)), stdout(pipe(Out))]),
-    close(In),
+                   [stdin(null), stdout(pipe(Out))]),
     read_process(Out, X0),
     split_string(X0, "", " \r\n", [X]).
 test(read_error, [condition(has_exe(sh)),X == 'error\n']) :-
